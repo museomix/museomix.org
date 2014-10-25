1ce664591355a7b7c496f61cecd3d89df76462f7
Samy Rabih <contact@lahaut.info>
2014-10-25 19:33:14 +0200

    Adjustement of responsiveness and page width

--- wp-content/themes/museomix-design-2/Page_template.php
+++ wp-content/themes/museomix-design-2/Page_template.php
@@ -89,7 +89,7 @@ if(!$ContenuPage)
 	$ContenuPage = apply_filters('the_content',get_the_content());
 		
 ?>
-<div class="bloc-page span12 centered">
+<div class="bloc-page <?php echo ((is_front_page() || (get_field('pleine_largeur'))) ? 'span12' : 'span9'); ?> centered">
 
 	<div class="contenu-page">
 	
